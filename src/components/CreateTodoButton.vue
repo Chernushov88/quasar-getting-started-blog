6b01fed672e0ac0f4037e706dc7a9b41b03a19ed
chernushov88@gmail.com <chernushov88@gmail.com>
2023-06-24 17:07:16 +0100

    Quasar Plugins

--- src/components/CreateTodoButton.vue
+++ src/components/CreateTodoButton.vue
@@ -1,12 +1,67 @@
 <template>
-  <q-btn />
+  <q-btn @click="handleButtonClicked" />
 </template>
 
 <script>
+// import { ref, computed } from 'vue'
+// import { useQuasar } from 'quasar'
+
 export default {
-  // name: 'ComponentName',
-  setup () {
-    return {}
+  // setup () {
+  //   const $q = useQuasar()
+  //   const moreContent = ref(true)
+  //
+  //   return {
+  //     createTodo () {
+  //       $q.notify({
+  //         title: 'Create Todo',
+  //         prompt: {
+  //           model: '',
+  //           type: 'text'
+  //         }
+  //       })
+  //     },
+  //     handleButtonClicked () {
+  //       $q.dialog({
+  //         title: 'Create Todo',
+  //         prompt: {
+  //           model: '',
+  //           type: 'text'
+  //         }
+  //       })
+  //     }
+  //   }
+  // }
+  // }
+  methods: {
+    handleButtonClicked () {
+      this.$q.dialog({
+        title: 'Create Todo',
+        prompt: {
+          model: '',
+          type: 'text'
+        }
+      })
+        .onOk(this.createTodo)
+    },
+    createTodo (data) {
+      this.chekedValue(data)
+    },
+    chekedValue (value) {
+      if (value.length > 3) {
+        this.$q.notify({
+          message: 'Todo Created!',
+          icon: 'mdi-check',
+          color: 'positive'
+        })
+      } else {
+        this.$q.notify({
+          message: `Error Todo is note ${value}`,
+          icon: 'mdi-account',
+          color: 'negative'
+        })
+      }
+    }
   }
 }
 </script>
