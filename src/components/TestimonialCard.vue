dcb586223074a20de11d8a456fe865e1fe8c1283
chernushov88@gmail.com <chernushov88@gmail.com>
2023-06-26 20:58:33 +0100

    Testimonial Section

---
+++ src/components/TestimonialCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-card
+    class="q-pa-lg full-width text-center column items-center"
+    style="max-width: 350px;"
+    :style="{
+      borderBottom: `6px solid ${getPaletteColor('purple-4')}`
+    }"
+  >
+    <q-img
+      style="max-width: 180px; border-radius: 50%;"
+      :src="imgSrc"
+    />
+    <div><strong>Ukrainian Insurgent Army</strong></div>
+    <div>
+      The Ukrainian Insurgent Army (Ukrainian: Українська повстанська армія, УПА, romanized: Ukrayins'ka Povstans'ka Armiia, abbreviated UPA) was a Ukrainian nationalist paramilitary and partisan formation founded by the Organization of Ukrainian Nationalists on October 14, 1942.[1] During World War II, it was engaged in guerrilla warfare against the Soviet Union, the Polish Underground State, Communist Poland, and Nazi Germany.[2]
+    </div>
+  </q-card>
+</template>
+
+<script>
+import { colors } from 'quasar'
+const { getPaletteColor } = colors
+
+export default {
+  // name: 'ComponentName',
+  setup () {
+    const imgSrc = require('assets/upa.jpg')
+
+    return {
+      imgSrc,
+      getPaletteColor
+    }
+  }
+}
+</script>
